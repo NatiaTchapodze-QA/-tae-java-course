8399ffacd54f22593a48e2ce7ec7aee9ad14f3a2
Natia Tchapodze <nchapodze@credo.ge>
2026-06-05 17:27:56 +0400

    [Natia T.] HW6 — Animal & Dog

---
+++ src/main/java/homework6/Animal.java
@@ -0,0 +1,15 @@
+/** @author <ნათია ჭაფოძე> */
+package homework6;
+
+public class Animal {
+
+    protected String name;
+    public Animal(String name) {
+        this.name=name;
+    }
+
+    public void makeSound() {
+        System.out.println("Some Sound");
+    }
+}
+
---
+++ src/main/java/homework6/Dog.java
@@ -0,0 +1,18 @@
+/** @author <ნათია ჭაფოძე> */
+package homework6;
+
+public class Dog extends Animal {
+
+    public Dog(String name) {
+        super(name);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println(name + ": Bark!");
+    }
+
+}
+
+
+
