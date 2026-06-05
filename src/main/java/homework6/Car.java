d919c6687af9e87f6e0a5e08020e8e8dc3de5c7d
Natia Tchapodze <nchapodze@credo.ge>
2026-06-05 17:31:07 +0400

    [Natia T.] HW6 — Vehicle & Car

---
+++ src/main/java/homework6/Car.java
@@ -0,0 +1,17 @@
+/** @author <ნათია ჭაფოძე> */
+package homework6;
+
+public class Car extends Vehicle {
+
+    int doors;
+    public Car(String brand, int year, int doors) {
+        super(brand, year);
+        this.doors = doors;
+}
+
+@Override
+    public void info ()
+{
+    System.out.println(brand + " " + year + ", " + doors + " doors");
+}
+}
