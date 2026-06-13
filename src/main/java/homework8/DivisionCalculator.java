4a4f4ed905cbe267894c69320f67c67a5e5e68f0
Natia Tchapodze <nchapodze@credo.ge>
2026-06-13 15:59:29 +0400

    [Natia T.] HW8 — DivisionCalculator

---
+++ src/main/java/homework8/DivisionCalculator.java
@@ -0,0 +1,22 @@
+/** @author <Natia Tchapodze> */
+package homework8;
+
+public class DivisionCalculator {
+
+    public static int safeDivide ( int a, int b) {
+        try {
+            return a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("0-ზე გაყოფა ");
+            return 0;
+        }
+    }
+    public static void main (String[] args) {
+        System.out.println(safeDivide(10, 2));
+        System.out.println(safeDivide(5, 0));
+}
+
+}
+
+
+
